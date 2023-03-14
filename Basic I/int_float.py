007895318d436c06413e59e822300f5d8e097200
Gamerz Choices <admin@gamerzchoices.com>
2023-03-15 00:57:46 +0545

    first commit

---
+++ Basic I/int_float.py
@@ -0,0 +1,11 @@
+print(2 + 6)
+print(6 - 2)
+print(3 * 7)
+print(6 / 1)
+print(20 % 5)  # Modulos
+print(4 ** 6)  # Exponent
+print(7 // 3)  # Floor Division
+
+# Type
+print(type(7.634))
+print(type(4*23))
---
+++ Basic I/math_function.py
@@ -0,0 +1,9 @@
+import math
+
+print(round(35.36))
+print(abs(-636))
+print(pow(2, 8))
+
+print(math.sqrt(64))
+print(math.ceil(10.1))
+print(math.floor(10.1))
